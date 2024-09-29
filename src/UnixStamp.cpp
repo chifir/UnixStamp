b6541e4e0308ab1a3c489328ab4fe310c185a87b
4ifir <4ifirus@gmail.com>
2024-09-29 04:39:54 +0300

    1) Added tests for unixToCivbil

--- src/UnixStamp.cpp
+++ src/UnixStamp.cpp
@@ -27,10 +27,10 @@ unixstamp UnixStamp::convertTimeToUnix(civil_time time)
     uint16_t year = time.year;
     uint8_t month = time.mon;
     year -= month <= 2;
-    uint16_t era = ((year >= 0 ? year : (year - YEARS_IN_ERA - 1)) / YEARS_IN_ERA);
-    uint16_t yearOfEra = year - era * YEARS_IN_ERA;
-    uint16_t dayOfYear = ((DAYS_AFTER_FIRST_MARCH * (month > 2 ? month - 3 : month + 9) + 2)/5) + time.day - 1;
-    uint32_t dayOfEra = (yearOfEra * 36524)/ 100 + dayOfYear + 1;
+    uint8_t era = ((year >= 0 ? year : (year - YEARS_IN_ERA - 1)) / YEARS_IN_ERA);
+    uint32_t yearOfEra = year - era * YEARS_IN_ERA;
+    uint32_t dayOfYear = ((DAYS_AFTER_FIRST_MARCH * (month > 2 ? month - 3 : month + 9) + 2)/5) + time.day - 1;
+    uint32_t dayOfEra = ((uint32_t)(yearOfEra * 365)) - ((uint32_t)(yearOfEra / 100)) + ((uint32_t)(yearOfEra / 4)) + dayOfYear;
     uint32_t days = (era  * DAYS_IN_ERA) + dayOfEra - DAYS_BEFORE_UNIX;
     uint32_t unix = days * ONE_DAY_IN_SEC + (((uint32_t)(time.hour - time.tz)) * ONE_HOUR_IN_SEC) + ((uint32_t)(time.min * 60)) + time.sec;
     return unix;
@@ -40,10 +40,10 @@ civil_time UnixStamp::convertUnixToTime(unixstamp unix, int8_t tz)
 {
     civil_time time;
     uint64_t eraStamp;
-    uint16_t era, dayOfEra, yearOfEra, dayOfYear, yearFromEra, monthFromDayOfYear;
+    uint32_t era, dayOfEra, yearOfEra, dayOfYear, yearFromEra, monthFromDayOfYear;
     uint8_t month;
 
-    unix += tz * ONE_HOUR_IN_SEC;
+    unix -= tz * ONE_HOUR_IN_SEC;
     time.sec = unix % ONE_MINUTE_IN_SEC;
     unix /= ONE_MINUTE_IN_SEC;
     time.min = unix % ONE_HOUR_IN_MIN;
--- test/avr/test_lib/UnixStampTestAvr.cpp
+++ test/avr/test_lib/UnixStampTestAvr.cpp
@@ -3,7 +3,11 @@
 #include <stdint.h>
 
 #define EXPECETED_CHERNOBYL_TIMESTAMP 514848225
-#define EXPECTED_ERROR -1
+#define EXPECTED_ERA_END 951868799 
+#define EXPECTED_ERA_START 951868800 
+#define EXPECTED_EPOCH_LEFT_BORDER 0
+#define EXPECTED_EPOCH_RIGHT_BORDER 4294967295
+
 
 void setUp(void) {}
 
@@ -21,24 +25,89 @@ civil_time fillTimeStruct(int16_t year, int8_t mon, int8_t day, int8_t hour, int
     return time;
 }
 
-void test_conversion_to_unix_unix_era_start()
+void verify_date_conversion_from_civil_to_epoch(uint32_t expected_unix, int16_t year, int8_t mon, int8_t day, int8_t hour, int8_t min, int8_t sec, int8_t tz) 
 {
-    civil_time time = fillTimeStruct(1970, 1,  1, 0, 0, 0, 0);
+    civil_time time = fillTimeStruct(year, mon, day, hour, min, sec, tz);
     unixstamp unix = UnixStamp::convertTimeToUnix(time);
-    TEST_ASSERT_EQUAL(0, unix);
+    TEST_ASSERT_EQUAL(expected_unix, unix);
+}
+
+void verify_date_conversion_from_epoch_to_civil(int16_t expected_year, int8_t expected_mon, int8_t expected_day, int8_t expected_hour, int8_t expected_min, int8_t expected_sec, int8_t expected_tz, uint32_t unix, int8_t tz)
+{
+    civil_time expected_time = fillTimeStruct(expected_year, expected_mon, expected_day, expected_hour, expected_min, expected_sec, expected_tz);
+    civil_time actual_time = UnixStamp::convertUnixToTime(unix, tz);
+    TEST_ASSERT_EQUAL(expected_time.year, actual_time.year);
+    TEST_ASSERT_EQUAL(expected_time.mon, actual_time.mon);
+    TEST_ASSERT_EQUAL(expected_time.day, actual_time.day);
+    TEST_ASSERT_EQUAL(expected_time.hour, actual_time.hour);
+    TEST_ASSERT_EQUAL(expected_time.min, actual_time.min);
+    TEST_ASSERT_EQUAL(expected_time.sec, actual_time.sec);
+}
+
+void test_conversion_to_unix_unix_left_border()
+{
+    verify_date_conversion_from_civil_to_epoch(EXPECTED_EPOCH_LEFT_BORDER, 1970, 1, 1, 0, 0, 0, 0);
+}
+
+void test_conversion_to_unix_unix_right_border()
+{
+    verify_date_conversion_from_civil_to_epoch(EXPECTED_EPOCH_RIGHT_BORDER, 2106, 2, 7, 6, 28, 15, 0);
 }
 
 void test_conversion_to_unix_chernobyl()
 {
-    civil_time time = fillTimeStruct(1986, 4,  25, 21, 23, 45, 0);
-    unixstamp unix = UnixStamp::convertTimeToUnix(time);
-    TEST_ASSERT_EQUAL(EXPECETED_CHERNOBYL_TIMESTAMP, unix);
+    verify_date_conversion_from_civil_to_epoch(EXPECETED_CHERNOBYL_TIMESTAMP, 1986, 4, 25, 21, 23, 45, 0);
 }
+
+void test_conversion_to_unix_calendar_era_end()
+{
+    verify_date_conversion_from_civil_to_epoch(EXPECTED_ERA_END, 2000, 2, 29, 23, 59, 59, 0);
+}
+
+void test_conversion_to_unix_calendar_era_start()
+{
+    verify_date_conversion_from_civil_to_epoch(EXPECTED_ERA_START, 2000, 3, 1, 0, 0, 0, 0);
+}
+
+void test_conversion_to_civil_unix_left_border()
+{
+    verify_date_conversion_from_epoch_to_civil(1970, 1, 1, 0, 0, 0, 0, EXPECTED_EPOCH_LEFT_BORDER, 0);
+}
+
+void test_conversion_to_civil_unix_right_border()
+{
+    verify_date_conversion_from_epoch_to_civil(2106, 2, 7, 6, 28, 15, 0, EXPECTED_EPOCH_RIGHT_BORDER, 0);
+}
+
+void test_conversion_to_civil_chernobyl()
+{
+    verify_date_conversion_from_epoch_to_civil(1986, 4, 25, 21, 23, 45, 0, EXPECETED_CHERNOBYL_TIMESTAMP, 0);
+}
+
+void test_conversion_to_civil_calendar_era_end()
+{
+    verify_date_conversion_from_epoch_to_civil(2000, 2, 29, 23, 59, 59, 0, EXPECTED_ERA_END, 0);
+}
+
+void test_conversion_to_civil_calendar_era_start()
+{
+    verify_date_conversion_from_epoch_to_civil(2000, 3, 1, 0, 0, 0, 0, EXPECTED_ERA_START, 0);
+}
+
 
 int main(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_conversion_to_unix_unix_era_start);
+    RUN_TEST(test_conversion_to_unix_unix_left_border);
+    RUN_TEST(test_conversion_to_unix_unix_right_border);
     RUN_TEST(test_conversion_to_unix_chernobyl);
+    RUN_TEST(test_conversion_to_unix_calendar_era_end);
+    RUN_TEST(test_conversion_to_unix_calendar_era_start);
+
+    RUN_TEST(test_conversion_to_civil_unix_left_border);
+    RUN_TEST(test_conversion_to_civil_unix_right_border);
+    RUN_TEST(test_conversion_to_civil_chernobyl);
+    RUN_TEST(test_conversion_to_civil_calendar_era_end);
+    RUN_TEST(test_conversion_to_civil_calendar_era_start);
     return UNITY_END();
 }
